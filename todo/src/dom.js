07e25b5860d1db045f4815ee24ae5a2188389aad
SalmonBoyFred <michaelfdow025@gmail.com>
2023-08-09 22:18:43 -0400

    add popup form for todo item

--- todo/src/dom.js
+++ todo/src/dom.js
@@ -2,8 +2,20 @@ import todoArray from './index.js';
 import { addItemToProject} from './index.js';
 
 const todoButton = document.getElementById("addTodoButton");
+const cancelBtn = document.getElementById("closeForm");
 
-todoButton.addEventListener("click", addItemToProject);
+todoButton.addEventListener("click", openTheForm);     //function should pop up form for todo
+
+cancelBtn.addEventListener("click", closeTheForm);
+
+function openTheForm() {
+    document.getElementById("popupForm").style.display = "block";
+};
+  
+function closeTheForm() {
+    document.getElementById("popupForm").style.display = "none";
+};
+  
 
 
 
